2a35a7a6cb6e10b154fde75b3deba291b5886619
Arthur V Fain <arthurvfain@gmail.com>
2021-05-06 00:06:48 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const yourName = `Arthur`;
+document.getElementById('main').remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = `${yourName} is the champion`;
+document.body.appendChild(newHeader);
